3644612bbc9fa725b666b3fd1e308454f570499c
Harish_Raghav <143773862+Harish2902@users.noreply.github.com>
2024-08-13 20:30:10 +0530

    Delete Pl_Sql directory

--- Pl_Sql/Triggers/Senario1.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE TRIGGER UpdateCustomerLastModified
-BEFORE UPDATE ON Customers
-FOR EACH ROW
-BEGIN
-    -- Update the LastModified column to the current date and time
-    :NEW.LastModified := SYSDATE;
-END;
-/
